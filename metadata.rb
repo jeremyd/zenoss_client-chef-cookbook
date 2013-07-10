af41ef47efb544a46801067c7ab5d53e98493595
Dave Petzel <david.petzel@disney.com>
2013-07-09 20:33:10 -0400

    bumping version for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "davidpetzel@gmail.com"
 license          "Apache 2.0"
 description      "Configures a node to be a Zenoss client/monitored node"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 supports "ubuntu"
 supports  "rhel"
